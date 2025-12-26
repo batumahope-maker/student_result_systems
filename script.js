99258c6b18104b15494e918887b07012792a170b
batumahope-maker <batumahope@gmail.com>
2025-12-26 09:19:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,13 +24,13 @@ const students = [
         registration_number: "STU001",
         password: "1234",
         name: "Alice Uwase",
-        results: { Math: 85, English: 90, Science: 78 }
-    },
+        results: { Math: 75, English: 88, Website: 82,  JavaScript: 80, CVC: 95, Graphic: 89, },
     {
         registration_number: "STU002",
         password: "5678",
         name: "Bob Kamau",
-        results: { Math: 75, English: 88, Science: 82 }
+        results: { Math: 75, English: 88, Website: 82,  JavaScript: 80, CVC: 95, Graphic: 89, }
+
     }
 ];
 
